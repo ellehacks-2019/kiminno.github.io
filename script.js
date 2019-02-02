a0e17a907d6306db80375e252e27bfbc03ba5391
Jennthedragon <47278187+Jennthedragon@users.noreply.github.com>
2019-02-02 13:28:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const syncButton = document.getElementsByClassName('syncButton');
+
 let isSyncing = [false];
+
 console.log(syncButton);
+
+syncButton.addEventListener ('click', function (){
+  console.log("clicked");
+  syncButton.classList.toggle('pushed');
+  document.getElementsByClassName("syncButton").disabled = true;
+  }
+})
